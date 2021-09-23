254cc3913bef90eec473bd65683a061cdeb0b88b
Matheus Alexandre <alexandremhm@gmail.com>
2021-09-23 16:12:51 -0300

    add script velocidade

--- Bloco-26/26.1/ex-dia-1/imc.js
+++ Bloco-26/26.1/ex-dia-1/imc.js
@@ -6,6 +6,32 @@ const weight = readline.questionFloat('Qual seu peso em kg?');
 const imcCalculator = (height, weight) => {
   const imc = (weight/ Math.pow(height / 100, 2)).toFixed(2);
   console.log(`O seu IMC é: ${imc}`);
+
+  if (imc < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
+  }
+
+  if (imc >= 18.5 && imc < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+
+  if (imc >= 25 && imc < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+
+  if (imc >= 30 && imc < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+
+  if (imc >= 35 && imc < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }
+  console.log('Situação: Obesidade graus III e IV');
 };
 
 imcCalculator(height, weight);
